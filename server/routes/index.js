f8dff039d74509ee9390d7c03685af9f98da6a05
huynhsamha <huynhsamha@gmail.com>
2018-06-03 14:50:54 +0700

    fix format view

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,18 +1,21 @@
 import express from 'express';
-import moment from 'moment';
+import path from 'path';
 
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
-  res.send('Hello freeCodeCamp challenge! Please try <a href="/api/whoami"/>/api/whoami</a>');
+  res.sendFile(path.join(__dirname, '../views/index.html'));
 });
 
 router.get('/api/whoami', (req, res, next) => {
   const { headers, connection } = req;
 
-  const ipaddress = headers['x-forwarded-for'] || connection.remoteAddress || req.socket.remoteAddress || connection.socket.remoteAddress;
-  const software = headers['user-agent'].split(') ')[0].split(' (')[1];
-  const language = headers['accept-language'].split(',')[0];
+  const ipaddress = req.headers['x-forwarded-for'] ||
+    req.connection.remoteAddress ||
+    req.socket.remoteAddress ||
+    (req.connection.socket ? req.connection.socket.remoteAddress : null);
+  const software = headers['user-agent'];
+  const language = headers['accept-language'];
 
   res.send({ ipaddress, language, software });
 });
